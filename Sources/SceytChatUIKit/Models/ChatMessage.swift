461e7a0c1999f04f8e4c0b7bd9614febec6d0ca5
Sargis Mkhitaryan <sargis.mkhitaryan@varmtech.com>
2025-12-25 17:27:14 +0400

    Add property to enable/disable view once

--- Sources/SceytChatUIKit/Models/ChatMessage.swift
+++ Sources/SceytChatUIKit/Models/ChatMessage.swift
@@ -148,8 +148,7 @@ public class ChatMessage {
         replyCount = Int(dto.replyCount)
         displayCount = Int(dto.displayCount)
         disableMentionsCount = dto.disableMentionsCount
-//        viewOnce = dto.viewOnce
-        viewOnce = true
+        viewOnce = dto.viewOnce
         markerCount = dto.markerTotal
         if let user = dto.user {
             self.user = user.convert()
--- Sources/SceytChatUIKit/Views/Message/MessageInputViewController.swift
+++ Sources/SceytChatUIKit/Views/Message/MessageInputViewController.swift
@@ -104,7 +104,13 @@ open class MessageInputViewController: ViewController, UITextViewDelegate {
             updateState()
         }
     }
-    
+
+    open var isViewOnceFeatureEnabled = false {
+        didSet {
+            updateState()
+        }
+    }
+
     open var cameraButtonWidth: CGFloat { 48.0 }
     open var recordButtonWidth: CGFloat { 52.0 }
     open var sendButtonWidth: CGFloat { 52.0 }
@@ -352,7 +358,7 @@ open class MessageInputViewController: ViewController, UITextViewDelegate {
     
     open func updateTrailingInputButtons() {
         let shouldShowSendButton = !inputTextView.text.replacingOccurrences(of: "\u{fffc}", with: "").trimmingCharacters(in: .whitespacesAndNewlines).isEmpty || !selectedMediaView.items.isEmpty
-        let shouldShowViewOnceButton = selectedMediaView.items.count == 1
+        let shouldShowViewOnceButton = isViewOnceFeatureEnabled && selectedMediaView.items.count == 1
 
         // Update button visibility
         sendButton.isHidden = !shouldShowSendButton
@@ -427,7 +433,7 @@ open class MessageInputViewController: ViewController, UITextViewDelegate {
     }
 
     open func updateViewOnceButtonVisibility() {
-        let shouldShow = selectedMediaView.items.count == 1
+        let shouldShow = isViewOnceFeatureEnabled && selectedMediaView.items.count == 1
 
         // Reset state when conditions change
         if !shouldShow && isViewOnceEnabled {
